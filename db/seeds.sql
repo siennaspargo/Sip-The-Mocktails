265b9bc023743d6893d092f5c33762eff6ea3de9
SiennaS <ssienna48@gmail.com>
2018-12-05 17:19:59 -0500

    mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,37 +1,40 @@
 -- In this file, write insert queries to populate the burgers table with at least three entries.
 
 -- write code here
+-- DROP DATABASE IF EXISTS mocktails_db;
+-- CREATE DATABASE mocktails_db;
+-- USE mocktails_db;
 
+-- CREATE TABLE mocktails (
+--   id int NOT NULL AUTO_INCREMENT,
+--   mocktail_name VARCHAR(30) NOT NULL,
+--   devoured BOOLEAN DEFAULT true,
+--   PRIMARY KEY (id)
+-- );
 
   INSERT INTO mocktails
     (mocktail_name)
   VALUES
     ("Shirly Ginger");
 
-  INSERT INTO schools
-    (name)
+  INSERT INTO mocktails
+    (mocktail_name)
   VALUES
     ("Tahitian Coffee");
-
-  INSERT INTO schools
-    (name)
+  INSERT INTO mocktails
+    (mocktail_name)
   VALUES
     ("Strawberry Fields");
     
-  INSERT INTO schools
-    (name)
+  INSERT INTO mocktails
+    (mocktail_name)
   VALUES
     ("Watermelon MargNArita");
 
-  INSERT INTO schools
-    (name)
+  INSERT INTO mocktails
+    (mocktail_name)
   VALUES
     ("Mango Mule");
-
-
-
-
-
 
 
 
@@ -43,6 +46,8 @@
 -- Start MySQL command line tool and login: mysql -u root -p.
 
 -- With the mysql> command line tool running, enter the command source schema.sql. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
+
+
 
 -- Now insert the entries you defined in seeds.sql by running the file: source seeds.sql.
 
